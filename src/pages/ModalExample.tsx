e7eacd5162c39a331b8a940c2d5d1f316f3334b2
ldikmans <lonneke.dikmans@vennster.nl>
2020-10-16 16:13:57 +0200

    1 file modified

--- src/pages/ModalExample.tsx
+++ src/pages/ModalExample.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonModal, IonPage } from '@ionic/react';
+import { IonButton, IonContent, IonModal, IonPage } from '@ionic/react';
 import React, { useState } from 'react';
 import Posts from '../components/Posts';
 import ModalHeader from '../components/ModalHeader';
@@ -17,9 +17,12 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
 
   return (
     <IonPage>
+    <IonContent>
       <IonButton onClick={() => setShowModal(true)}>Open Modal</IonButton>
       <IonButton onClick={() => {setShowModal(true); setThrowError(true)}}> Open Modal that throws and error and closes</IonButton>
-      <IonModal isOpen={showModal} 
+     
+    </IonContent>
+     <IonModal isOpen={showModal} 
       cssClass='modal'
       onDidDismiss={()=> {hideModal()}}>
         <ModalHeader title="This is a modal with a header that can close" setShowModal={setShowModal} />
