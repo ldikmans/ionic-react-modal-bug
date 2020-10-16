157358852c1be5532a2415aa4aa7142294bfe3be
Lonneke Dikmans <lonneke.dikmans@vennster.nl>
2020-10-16 16:50:44 +0200

    Revert "simplified routing"
    This reverts commit 3fe76158893de81afa5176d337abc150d958ecc9.

---
+++ src/components/DynamicModal.tsx
@@ -0,0 +1,38 @@
+import { IonButton, IonItem, IonLabel, IonModal } from '@ionic/react';
+import React from 'react';
+import { useHistory } from 'react-router';
+import './ExploreContainer.css';
+import ModalHeader from './ModalHeader';
+
+interface ContainerProps {
+    open: boolean,
+    setShowModal: Function
+ }
+
+const DynamicModal: React.FC<ContainerProps> = ({open, setShowModal}) => {
+
+  const history = useHistory();
+
+  function goToHomePage() {
+    setShowModal(false);
+    history.replace('/', {direction: 'back'});
+  }
+
+  return (
+      <IonModal 
+        isOpen={open}
+        swipeToClose = {true}
+        cssClass = 'modal'
+      >
+        <ModalHeader title="This is content from a separate modal" setShowModal={setShowModal}/>
+        <IonItem>
+          <IonLabel>
+            Click this button to go to the home page
+            </IonLabel>
+          <IonButton onClick={() => { goToHomePage() }}>Go to home page</IonButton>
+        </IonItem>
+      </IonModal>
+  );
+};
+
+export default DynamicModal;
